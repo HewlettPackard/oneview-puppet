478b0266c769f93b29b7dc751fb6767af1e39163
Ana Raquel Campesan <ana-raquel.campesan@hpe.com>
2016-05-19 12:16:16 -0400

    added comments

--- lib/puppet/parser/functions/ethernet_network_create.rb
+++ lib/puppet/parser/functions/ethernet_network_create.rb
@@ -2,7 +2,7 @@ require 'oneview-sdk'
 
 module Puppet::Parser::Functions
     newfunction(:ethernet_network_create) do |args|
-
+#
 
     options = args[0] ## Creates the Hash "options" using the arguments received
     options.each { |key,value| options[key] = nil if value == 'nil' } ## Removes quotes from nil values inside the hash
