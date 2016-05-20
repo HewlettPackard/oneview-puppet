a888e3d9da58de58c48c6ad1e0fdb67a42c73006
fgbulsoni <fgbulsoni@hpe.com>
2016-05-20 09:33:03 -0300

    Changes to update so it detects if a network with the name to be updated already exists. Idempotency fix.

--- lib/puppet/parser/functions/ethernet_network.rb
+++ lib/puppet/parser/functions/ethernet_network.rb
@@ -12,18 +12,26 @@ module Puppet::Parser::Functions
     options.each { |key,value| options[key] = false if value == 'false' } ## Removes quotes from false values inside the hash
 
     # Third set of values, only used in updates
+    attributes = Hash.new
     attributes_exist = false
     attributes_exist = true if args[2]
     attributes = args[2] if attributes_exist # new attributes to be updated
     attributes.each { |key,value| attributes[key] = nil if value == 'nil' } if attributes_exist ## Removes quotes from nil values inside the hash
     attributes.each { |key,value| attributes[key] = false if value == 'false' } if attributes_exist ## Removes quotes from false values inside the hash
-
-
+    attributes_has_name = false
+    attributes_has_name = true if attributes['name']
+    new_network_exists = false
+    new_matches = OneviewSDK::EthernetNetwork.find_by(@client, name: attributes['name'])
+    new_network_exists = true if new_matches.first
 
     # Validates if a network with specified name exists
     network_exists = false
     matches = OneviewSDK::EthernetNetwork.find_by(@client, name: options['name'])
     network_exists = true if matches.first
+    # Validation specific to Update
+    #new_matches = OneviewSDK::EthernetNetwork.find_by(@client, name: attributes['name']) if attributes['name']
+    #puts new_matches if attributes['name']
+    #new_network_exists = true if new_matches.first && action == "update" && attributes_exist && attributes['name']
 
     #===========================================================================
 
@@ -32,7 +40,6 @@ module Puppet::Parser::Functions
       ethernet = OneviewSDK::EthernetNetwork.new(@client, options)
       ethernet.create unless ethernet.retrieve!
       puts "\nCreated ethernet-network '#{ethernet[:name]}' sucessfully.\n  uri = '#{ethernet[:uri]}'" if !network_exists
-
       puts "\nAn ethernet network named #{options['name']} already exists.\n" if network_exists # in case the ethernet network already exists
 
     when "find"
@@ -40,11 +47,12 @@ module Puppet::Parser::Functions
       puts "\nAn ethernet network named '#{options['name']}' does not exist.\n" if !network_exists
 
     when "update"
-      matches.first.update(attributes) if network_exists && attributes_exist
+      matches.first.update(attributes) if network_exists && attributes_exist && !new_network_exists
       puts "\n'attributes' not found. Please declare the attributes to be changed when using 'update'" if !attributes_exist
       puts "\nAn ethernet network named '#{options['name']}' does not exist.\n" if !network_exists
-      puts "\nUpdated ethernet-network: '#{options["name"]}'.\n  uri = '#{matches.first[:uri]}'" if network_exists && attributes_exist
-      puts "with attributes: #{attributes}\n" if network_exists && attributes_exist
+      puts "\nAn ethernet network named '#{attributes['name']}' already exists, cannot change existing name to one that is already created.\n" if new_network_exists
+      puts "\nUpdated ethernet-network: '#{options["name"]}'.\n  uri = '#{matches.first[:uri]}'" if network_exists && attributes_exist && !new_network_exists
+      puts "with attributes: #{attributes}\n" if network_exists && attributes_exist && !new_network_exists
 
     when "delete" # WARNING:: delete works but returns an unknown error. to be fixed. error is most likely to be in the puts below
       matches.first.delete if network_exists
