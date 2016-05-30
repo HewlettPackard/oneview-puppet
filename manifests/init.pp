6605cc3d77147edef45a99964fc29c546b977096
fgbulsoni <fgbulsoni@hpe.com>
2016-05-30 10:49:54 -0300

    Changes to manifest handling to look more like playbook

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,40 @@
-class oneview {
+class oneview (
+  $action_list
+  ) {
+
+/*
+create_resources ($action_list)
+notify {"\n${key} and ${value}\n":}
+*/
 
         # # CONNECTION TO THE APPLIANCE
         # # **********************************************************************
+/* DEPRECATED CODE FOR LOGIN INFO
         # # Login info for the OneView appliance
         $login_information = {
-          appliance_adress =>  '<Your appliance IP here>',
-          login =>  '<Your appliance\'s user here>',
-          password =>  '<Your appliance\'s password here>'
+          appliance_adress =>  'https://172.16.103.21',
+          login =>  'Administrator',
+          password =>  'rainforest'
         }
+*/
         # # Connecting to appliance
-        connection_create($login_information)
+        $action_list.each |$key, $value| {
+          case $value['action'] {
+            'connection_create': {connection_create($value['options'])}
+            'ethernet_network_create': {ethernet_network("create", $value['options'])}
+            'ethernet_network_find': {ethernet_network("find", $value['options'])}
+            'ethernet_network_update': {ethernet_network("update", $value['options'], $value['attributes'])}
+            'ethernet_network_delete': {ethernet_network("delete", $value['options'])}
+            }
+        }
+
+
+/*
         #
         # # ETHERNET NETWORK
         # # **********************************************************************
         # # Ethernet options (going to be the same for all CRUD operations)
+
         $ethernet_options = {
                   vlanId =>  '1201',
                   purpose =>  'General',
@@ -31,4 +52,5 @@ class oneview {
         ethernet_network("find", $ethernet_options) # Finds ethernet network
         ethernet_network("update", $ethernet_options,$attributes) # Updates ethernet network
         ethernet_network("delete", $attributes) # Deletes ethernet network
+*/
 }
