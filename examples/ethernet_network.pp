33b735cd9a08c3f3c8d955737431ebaf314eba61
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-06-02 09:37:28 -0400

    added feature to update eth net name

--- examples/ethernet_network.pp
+++ examples/ethernet_network.pp
@@ -1,9 +1,9 @@
 oneview_ethernet_network{'net1':
     ensure => 'present',
     data => {
+      name                  => 'Puppet 2',
       vlanId                => '100',
       purpose               => 'General',
-      name                  => 'Puppet 1',
       smartLink             => 'false',
       privateNetwork        => 'true',
       connectionTemplateUri => 'nil',
@@ -14,25 +14,25 @@ oneview_ethernet_network{'net1':
 oneview_ethernet_network{'net2':
     ensure => 'present',
     data => {
+      name                  => 'Puppet 2',
       vlanId                => '1045',
       purpose               => 'Management',
-      name                  => 'Puppet 1',
-      smartLink             => 'false',
-      privateNetwork        => 'true',
+      smartLink             => 'true',
+      privateNetwork        => 'false',
       connectionTemplateUri => 'nil',
       type                  => 'ethernet-networkV3'
     }
 }
 
-oneview_ethernet_network{'net3':
-    ensure => 'absent',
-    data => {
-      vlanId                => '1050',
-      purpose               => 'General',
-      name                  => 'Puppet 1',
-      smartLink             => 'true',
-      privateNetwork        => 'true',
-      connectionTemplateUri => 'nil',
-      type                  => 'ethernet-networkV3'
-    }
-}
+# oneview_ethernet_network{'net3':
+#     ensure => 'absent',
+#     data => {
+#       vlanId                => '1050',
+#       purpose               => 'General',
+#       name                  => 'Puppet 1',
+#       smartLink             => 'true',
+#       privateNetwork        => 'true',
+#       connectionTemplateUri => 'nil',
+#       type                  => 'ethernet-networkV3'
+#     }
+# }
--- lib/puppet/provider/common.rb
+++ lib/puppet/provider/common.rb
@@ -21,33 +21,27 @@ end
 
 # Compares and updates the ethernet network if necessary
 def ethernet_network_update(resource, ethernet_network)
-  data = data_parse(resource)                                             # new data to be checked for changes
-  existing_ethernet_network = ethernet_network.first.data
-  if data['vlanId'].to_s != existing_ethernet_network['vlanId'].to_s            # checks whether the vlanId has been changed
+
+  data = data_parse(resource)
+  new_name_exists = true
+  if data['new_name']                                                                 # checks if name is within the attr to be changed
+    new_name = data['new_name']
+    new_name_exists = false if !get_ethernet_network(new_name).first                  # flag, true if exists
+    data.delete('new_name')
+  end
+  existing_ethernet_network = ethernet_network.first.data                             # new data to be checked for changes
+  if data['vlanId'].to_s != existing_ethernet_network['vlanId'].to_s                  # checks whether the vlanId has been changed
     Puppet.warning("Update operation does not support vlanId changing, ignoring...")
   end
-  data.delete('connectionTemplateUri') if data['connectionTemplateUri'] == nil  # the connectionTemplateUri can only be nil at its creation
-  data.delete('vlanId') if data['vlanId']                                       # in case a new vlanId exists, as it cannot be changed
-  data.delete('modified')                                                       # field not considered for comparison
-  updated_data = existing_ethernet_network.merge(data)                          # new hash / difference between old and new ones
-  if updated_data != existing_ethernet_network                                  # if there's any difference...
-    updated_data = Hash[updated_data.to_a-existing_ethernet_network.to_a]       # hash with different data remaining
-    ethernet_network.first.update(updated_data)                                 # the actual update on the
-    Puppet.warning("Updated: #{updated_data.inspect} ")                         # shows what
+  data.delete('connectionTemplateUri') if data['connectionTemplateUri'] == nil         # the connectionTemplateUri can only be nil at its creation
+  data.delete('vlanId') if data['vlanId']                                              # in case a new vlanId exists, as it cannot be changed
+  data.delete('modified')                                                              # field not considered for comparison
+  updated_data = existing_ethernet_network.merge(data)                                 # new hash / difference between old and new ones
+  updated_data['name'] = new_name if new_name_exists == false
+
+  if updated_data != existing_ethernet_network                                         # if there's any difference..
+    updated_data = Hash[updated_data.to_a-existing_ethernet_network.to_a]              # hash with different data remaining  # the actual update on the# hash with different data remaining
+    ethernet_network.first.update(updated_data)                                        # the actual update on the# hash with different data remaining
+    Puppet.notice("Updated: #{updated_data.inspect} ")
   end
 end
-
-# ============== --- methods ==============
-
-
-=begin  POSSIBLY WORTH DOING AN UPDATE_PARSE TO PARSE OPTIONS FOR UPDATE AND REMOVE NOT ALLOWED ONES
-def update_parse(resource_data)
-  data = resource['data']
-  data.each do |key,value|
-    data[key] = nil if value == 'nil'
-    data[key] = false if value == 'false'
-    data[key] = true if value == 'true'
-  end
-  return data
-end
-=end
