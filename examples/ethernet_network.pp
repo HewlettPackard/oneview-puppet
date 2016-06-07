f676a89d90f73ada78a58a6f35f96bb7f79caef9
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-06-07 13:51:19 -0400

    added query method -self.instances- (can be tested by typing 'puppet resource oneview_ethernet_network')

--- examples/ethernet_network.pp
+++ examples/ethernet_network.pp
@@ -1,7 +1,7 @@
-oneview_ethernet_network{'job':
-    ensure => 'present',
-        data   => ''
-}
+# oneview_ethernet_network{'job':
+#     ensure => 'present',
+#         data   => ''
+# }
 
 # oneview_ethernet_network{'job1':
 #     ensure => 'present',
@@ -20,34 +20,34 @@ oneview_ethernet_network{'job':
 #     }
 # }
 #
-# oneview_ethernet_network{'net1':
-#     ensure => 'present',
-#     require=> Oneview_ethernet_network['job2'],
-#     data   => {
-#       name                  => 'Puppet Network',
-#       vlanId                => '100',
-#       purpose               => 'General',
-#       smartLink             => 'false',
-#       privateNetwork        => 'true',
-#       connectionTemplateUri => 'nil',
-#       type                  => 'ethernet-networkV3'
-#     }
-# }
+oneview_ethernet_network{'net1':
+    ensure => 'present',
+    # require=> Oneview_ethernet_network['job2'],
+    data   => {
+      name                  => 'Puppet Network',
+      vlanId                => '100',
+      purpose               => 'General',
+      smartLink             => 'false',
+      privateNetwork        => 'true',
+      connectionTemplateUri => 'nil',
+      type                  => 'ethernet-networkV3'
+    }
+}
 #
-# oneview_ethernet_network{'net2':
-#     ensure  => 'present',
-#     require => Oneview_ethernet_network['net1'],
-#     data    => {
-#       name                  => 'Puppet Network',
-#       new_name              => 'Edited Name',
-#       vlanId                => '1045',
-#       purpose               => 'Management',
-#       smartLink             => 'true',
-#       privateNetwork        => 'false',
-#       connectionTemplateUri => 'nil',
-#       type                  => 'ethernet-networkV3'
-#     }
-# }
+oneview_ethernet_network{'net2':
+    ensure  => 'present',
+    require => Oneview_ethernet_network['net1'],
+    data    => {
+      name                  => 'Puppet Network',
+      new_name              => 'Edited Name',
+      vlanId                => '1045',
+      purpose               => 'Management',
+      smartLink             => 'true',
+      privateNetwork        => 'false',
+      connectionTemplateUri => 'nil',
+      type                  => 'ethernet-networkV3'
+    }
+}
 #
 # oneview_ethernet_network{'net3':
 #     ensure  => 'present',
