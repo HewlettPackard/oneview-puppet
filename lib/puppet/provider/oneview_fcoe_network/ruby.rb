ef71faf05ac89229db15477d03037d2bc0e30e30
Ana <ana.campesan@hpe.com>
2016-06-14 10:10:31 -0400

    splitting resource changes

--- lib/puppet/provider/oneview_fcoe_network/ruby.rb
+++ lib/puppet/provider/oneview_fcoe_network/ruby.rb
@@ -3,25 +3,18 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'common'))
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'fcoe_network'))
 require 'oneview-sdk'
 
-### FIXME: puppet parser is detecting an error on "Puppet::Type.type"
-### not entirely sure why, but should be fixed once found out.
 Puppet::Type.type(:oneview_fcoe_network).provide(:ruby) do
 
-  # Helper methods - TO BE REDEFINED
-  mk_resource_methods
-
   def initialize(*args)
-    super(*args)
-    @client = OneviewSDK::Client.new(login)
-    # puts "Running #{resource}"
-    # Puppet.notice("Connected to OneView appliance at #{@client.url}")
+      super(*args)
+      @client = OneviewSDK::Client.new(login)
   end
 
   def self.instances
     @client = OneviewSDK::Client.new(login)
     matches = OneviewSDK::FCoENetwork.get_all(@client)
     matches.collect do |line|
-      # Puppet.notice("FC Network: #{line['name']}, URI: #{line['uri']}")
+      # Puppet.notice("Ethernet Network: #{line['name']}, URI: #{line['uri']}")
       name = line['name']
       data = line.inspect
       new(:name   => name,
@@ -31,92 +24,59 @@ Puppet::Type.type(:oneview_fcoe_network).provide(:ruby) do
     end
   end
 
-  def self.prefetch(resources)
-    packages = instances
-    resources.keys.each do |name|
-      if provider = packages.find{ |pkg| pkg.name == name }
-        resources[name].provider = provider
-      end
-    end
-  end
-
-  # Provider methods
-
-  # TODO: decide whether an fc net can be created with a new name
   def exists?
-    # Gets the desired operation to perform
     state = resource['ensure'].to_s
-
-    # Verify ensure flag and sets environment flags for operations
     case state
-      when 'present' then
-        # Resource itself sends the name of the running proccess
-        fcoe_network = get_fcoe_network(resource['data']['name'])
-        fcoe_network_exists = false
-        fcoe_network_exists = true if fcoe_network.first
-        # Checking for and performing potential updates.
-        if fcoe_network_exists
-          Puppet.notice("#{resource} '#{resource['data']['name']}' located"+
-          " in Oneview Appliance")
-          fcoe_network_update(resource['data'], fcoe_network, resource)
-          return true
-        end
-      when 'absent' then
-        # Resource itself sends the name of the running proccess
-        fcoe_network = get_fcoe_network(resource['data']['name'])
-        fcoe_network_exists = false
-        fcoe_network_exists = true if fcoe_network.first
-        Puppet.notice("#{resource} '#{resource['data']['name']}' not located"+
-        " in Oneview Appliance") if fcoe_network_exists == false
-        return fcoe_network_exists
-      when 'found' then
-        fcoe_network = find_fcoe_networks(resource['data'])
-        fcoe_network_exists = false
-        fcoe_network_exists = true if fcoe_network
-        return fcoe_network_exists
+    when 'present' then
+      fcoe_network = get_fcoe_network(resource['data']['name'])
+      fcoe_network_exists = false
+      fcoe_network_exists = true if fcoe_network.first
+      if fcoe_network_exists
+        fcoe_network_update(resource['data'], fcoe_network, resource)
+        return true
+      end
+    when 'absent' then
+      # Resource itself sends the name of the running proccess
+      fcoe_network = get_fcoe_network(resource['data']['name'])
+      fcoe_network_exists = false
+      fcoe_network_exists = true if fcoe_network.first
+      return fcoe_network_exists
+    when 'found' then
+      fcoe_network = find_fcoe_networks(resource['data'])
+      fcoe_network_exists = false
+      fcoe_network_exists = true if fcoe_network
+      return fcoe_network_exists
     end
-    # puts @property_hash[:data]
     @property_hash[:ensure] == :present
   end
 
   def create
     data = data_parse(resource['data'])
     data.delete('new_name') if data['new_name']
-    fcoe_network = OneviewSDK::FCoENetwork.new(@client, data)
-    fcoe_network.create
+    fcoe_network = OneviewSDK::FCoENetwork.new(@client, data).create
     @property_hash[:ensure] = :present
-    @property_hash[:data] = data
   end
 
   def destroy
-    fcoe_network = get_fcoe_network(resource['data']['name'])
+    data = data_parse(resource['data'])
+    fcoe_network = get_fcoe_network(data['name'])
     fcoe_network.first.delete
     @property_hash.clear
   end
 
-  def data
-    @property_hash[:data]
-  end
-
-  def data=(value)
-    @property_hash[:data] = value
-  end
-
-
   def found
     # Searches networks with data matching the manifest data
     data = data_parse(resource['data'])
-    data.delete('connectionTemplateUri') if data['connectionTemplateUri'] == nil
     matches = OneviewSDK::FCoENetwork.find_by(@client, data)
     # If matches are found, iterate through them and notify. Else just notify.
-    unless matches.empty?
+    if matches
        matches.each { |network| Puppet.notice ( "\n\n Found matching network "+
       "#{network['name']} (URI: #{network['uri']}) on Oneview Appliance\n" ) }
-      true
+      return true
     else
-      Puppet.notice("\n\n No networks with the specified data were found on the "+
-      " Oneview Appliance\n")
-      false
+      Puppet.notice("No networks with the specified data were found on the "+
+      " Oneview Appliance")
+      return false
     end
   end
 
