da1d8dc5644304bca0e56e6b19a710ce44978041
Ana <ana.campesan@hpe.com>
2016-06-14 12:27:38 -0400

    -

--- examples/fcoe_network.pp
+++ examples/fcoe_network.pp
@@ -1,45 +1,33 @@
-# oneview_fcoe_network{'fcoe':
-#     ensure => 'present',
-#     data   => {
-#       name                  => 'FCoE Network',
-#       # new_name              => 'Test name', #This will be dropped at first
-#       vlanId                => '100',
-#       connectionTemplateUri => 'nil',
-#       type                  => 'fcoe-network'
-#     }
-# }
-#
-# oneview_fcoe_network{'fcoe update':
-#     ensure  => 'present',
-#     require => Oneview_fcoe_network['fcoe'],
-#     data    => {
-#       name                  => 'FCoE Network',
-#       new_name              => 'New FCoE Network Name',
-#       vlanId                => '100',
-#       connectionTemplateUri => 'nil',
-#       type                  => 'fcoe-network'
-#     }
-# }
-
-oneview_fcoe_network{'fcoe found':
-    ensure  => 'found',
-    # require => Oneview_fcoe_network['fcoe update'],
-    data    => {
-      name                  => 'FCoE',
+oneview_fcoe_network{'fcoe':
+    ensure => 'present',
+    data   => {
+      name                  => 'FCoE Network',
+      new_name              => 'Test name', #This will be dropped at first
       vlanId                => '100',
       connectionTemplateUri => 'nil',
       type                  => 'fcoe-network'
     }
 }
 
-#
-# oneview_fcoe_network{'fcoe update delete':
-#     ensure  => 'absent',
-#     require => Oneview_fcoe_network['fcoe update'],
-#     data    => {
-#       name                  => 'New FCoE Network Name',
-#       vlanId                => '100',
-#       connectionTemplateUri => 'nil',
-#       type                  => 'fcoe-network'
-#     }
-# }
+oneview_fcoe_network{'fcoe update':
+    ensure  => 'present',
+    require => Oneview_fcoe_network['fcoe'],
+    data    => {
+      name                  => 'FCoE Network',
+      new_name              => 'New FCoE Network Name',
+      vlanId                => '100',
+      connectionTemplateUri => 'nil',
+      type                  => 'fcoe-network'
+    }
+}
+
+oneview_fcoe_network{'fcoe update delete':
+    ensure  => 'absent',
+    require => Oneview_fcoe_network['fcoe update'],
+    data    => {
+      name                  => 'New FCoE Network Name',
+      vlanId                => '100',
+      connectionTemplateUri => 'nil',
+      type                  => 'fcoe-network'
+    }
+}
