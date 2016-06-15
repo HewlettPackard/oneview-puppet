87ee285716057c7bf01502fec826cc621ba6584d
Ana <ana.campesan@hpe.com>
2016-06-15 09:48:07 -0400

    fixed typos and added copyright notes to all fcoe net files

--- examples/fcoe_network.pp
+++ examples/fcoe_network.pp
@@ -1,4 +1,20 @@
-oneview_fcoe_network{'fcoe':
+################################################################################
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+oneview_fcoe_network{'FCoE Create':
     ensure => 'present',
     data   => {
       name                  => 'FCoE Network',
@@ -9,9 +25,9 @@ oneview_fcoe_network{'fcoe':
     }
 }
 
-oneview_fcoe_network{'fcoe update':
+oneview_fcoe_network{'FCoE Update':
     ensure  => 'present',
-    require => Oneview_fcoe_network['fcoe'],
+    require => Oneview_fcoe_network['FCoE Create'],
     data    => {
       name                  => 'FCoE Network',
       new_name              => 'New FCoE Network Name',
@@ -21,9 +37,21 @@ oneview_fcoe_network{'fcoe update':
     }
 }
 
-oneview_fcoe_network{'fcoe update delete':
+oneview_fcoe_network{'FCoE Found':
+    ensure  => 'found',
+    require => Oneview_fcoe_network['FCoE Update'],
+    data    => {
+      name                  => 'New FCoE Network Name',
+      vlanId                => '100',
+      connectionTemplateUri => 'nil',
+      type                  => 'fcoe-network'
+    }
+}
+
+
+oneview_fcoe_network{'FCoE Delete':
     ensure  => 'absent',
-    require => Oneview_fcoe_network['fcoe update'],
+    require => Oneview_fcoe_network['FCoE Found'],
     data    => {
       name                  => 'New FCoE Network Name',
       vlanId                => '100',
