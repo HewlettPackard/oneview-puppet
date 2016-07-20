4ed1119d4eaa76d303d7d45888cce15f74546052
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-07-20 07:56:32 -0400

    modified the LS unit test according to changes

--- spec/unit/provider/oneview_logical_switch_spec.rb
+++ spec/unit/provider/oneview_logical_switch_spec.rb
@@ -30,12 +30,23 @@ describe provider_class, unit: true do
     Puppet::Type.type(:oneview_logical_switch).new(
       name: 'LS',
       ensure: 'present',
-      ssh_username: 'dcs',
-      ssh_password: 'dcs',
-      snmp_port: '161',
-      community_string: 'public',
-      switch1_ip: '172.18.20.1',
-      switch2_ip: '172.18.20.2',
+      switches:
+      [
+        {
+          'ip' => '172.18.20.1',
+          'ssh_username' => 'dcs',
+          'ssh_password' => 'dcs',
+          'snmp_port' => '161',
+          'community_string' => 'public'
+        },
+        {
+          'ip' => '172.18.20.1',
+          'ssh_username' => 'dcs',
+          'ssh_password' => 'dcs',
+          'snmp_port' => '161',
+          'community_string' => 'public'
+        }
+      ],
       data:
           {
             'name' => 'LS',
@@ -47,17 +58,28 @@ describe provider_class, unit: true do
   context 'given the min parameters' do
     let(:resource) do
       Puppet::Type.type(:oneview_logical_switch).new(
-        name: 'ls',
+        name: 'LS',
         ensure: 'present',
-        ssh_username: 'dcs',
-        ssh_password: 'dcs',
-        snmp_port: '161',
-        community_string: 'public',
-        switch1_ip: '172.18.20.1',
-        switch2_ip: '172.18.20.2',
+        switches:
+        [
+          {
+            'ip' => '172.18.20.1',
+            'ssh_username' => 'dcs',
+            'ssh_password' => 'dcs',
+            'snmp_port' => '161',
+            'community_string' => 'public'
+          },
+          {
+            'ip' => '172.18.20.1',
+            'ssh_username' => 'dcs',
+            'ssh_password' => 'dcs',
+            'snmp_port' => '161',
+            'community_string' => 'public'
+          }
+        ],
         data:
             {
-              'name'                  => 'LS',
+              'name' => 'LS',
               'logicalSwitchGroupUri' => '/rest/'
             }
       )
