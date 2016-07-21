dee52096e2556f86a20386905320c51f575c1679
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-07-21 16:01:14 -0400

    file structure (- tests) for the resource

---
+++ lib/puppet/provider/oneview_network_set/ruby.rb
@@ -0,0 +1,45 @@
+################################################################################
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'login'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'common'))
+require 'oneview-sdk'
+
+Puppet::Type.type(:oneview_network_set).provide(:ruby) do
+  mk_resource_methods
+
+  def initialize(*args)
+    super(*args)
+    @client = OneviewSDK::Client.new(login)
+    @resourcetype = OneviewSDK::NetworkSet
+    @data = {}
+  end
+
+  def exists?
+    resource['data'] ? @data = resource['data'] : true
+    ns = @resourcetype.new(@client, name: @data['name'])
+    if ns.exists? && resource['ensure'] == :present
+      resource_update(@data, ns)
+    end
+    ns.exists?
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ lib/puppet/type/oneview_network_set.rb
@@ -0,0 +1,58 @@
+################################################################################
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+require_relative 'common'
+
+Puppet::Type.newtype(:oneview_network_set) do
+  desc "Oneview's Network Set"
+
+  # :nocov:
+  ensurable do
+    defaultvalues
+
+    newvalue(:found) do
+      provider.found
+    end
+
+    # GETs
+    newvalue(:get_connection_templates) do
+      provider.get_connection_templates
+    end
+
+    newvalue(:get_default_connection_template) do
+      provider.get_default_connection_template
+    end
+
+    newvalue(:get_schema) do
+      provider.get_schema
+    end
+  end
+
+  newparam(:name, namevar: true) do
+    desc 'Network Set name'
+  end
+  # :nocov:
+
+  newparam(:data) do
+    desc 'Network Set attributes'
+    validate do |value|
+      unless value.class == Hash
+        raise Puppet::Error, 'Inserted value for data is not valid'
+      end
+      uri_validation(value)
+    end
+  end
+end
