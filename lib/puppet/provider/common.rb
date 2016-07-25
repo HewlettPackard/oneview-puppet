c576e65bfd400cf2333ce15c3d0cebd383b98d2d
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-07-25 10:07:03 -0400

    common rubocop offense

--- lib/puppet/provider/common.rb
+++ lib/puppet/provider/common.rb
@@ -53,7 +53,7 @@ def resource_update(data, resourcetype)
   new_name_validation(data, resourcetype)
   raw_merged_data = current_attributes.merge(data)
   updated_data = Hash[raw_merged_data.to_a - current_attributes.to_a]
-  current_resource.update(updated_data) unless updated_data.empty?
+  current_resource.update(updated_data) if updated_data.size > 0
 end
 
 def new_name_validation(data, resourcetype)
