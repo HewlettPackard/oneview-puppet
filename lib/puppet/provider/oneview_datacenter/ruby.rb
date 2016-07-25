23f559b1238b43d8d82d6cbc491f0f996c844318
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-07-25 15:32:38 -0400

    implementation of datacenter

---
+++ lib/puppet/provider/oneview_datacenter/ruby.rb
@@ -0,0 +1,48 @@
+################################################################################
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'login'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'common'))
+require 'oneview-sdk'
+
+Puppet::Type.type(:oneview_datacenter).provide(:ruby) do
+  mk_resource_methods
+
+  def initialize(*args)
+    super(*args)
+    @client = OneviewSDK::Client.new(login)
+    @resourcetype = OneviewSDK::Datacenter
+    @data = {}
+  end
+
+  def exists?
+    return false unless resource['data']
+    @data = data_parse
+    @id = unique_id
+    dc = @resourcetype.new(@client, @id)
+    @exists = dc.retrieve!
+    resource_update(@data, @resourcetype) if @exists == true
+    @exists
+  end
+
+  def create
+    dc = @resourcetype.new(@client, @data).add
+  end
+
+  def destroy
+    @resourcetype.find_by(@client, @id).first.remove
+  end
+end
