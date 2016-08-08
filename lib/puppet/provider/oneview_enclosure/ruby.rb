5a6886dc1f39ebaeddce0091a036ef072fab8231
fgbulsoni <fgbulsoni@hpe.com>
2016-08-08 14:30:25 -0300

    Refactored enclosure resource to follow the standards defined in the project

--- lib/puppet/provider/oneview_enclosure/ruby.rb
+++ lib/puppet/provider/oneview_enclosure/ruby.rb
@@ -16,194 +16,112 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'login'))
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'common'))
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'enclosure'))
 require 'oneview-sdk'
 
 Puppet::Type.type(:oneview_enclosure).provide(:ruby) do
-
   mk_resource_methods
 
   def initialize(*args)
     super(*args)
     @client = OneviewSDK::Client.new(login)
+    @resourcetype = OneviewSDK::Enclosure
+    # Initializes the data so it is parsed only on exists and accessible throughout the methods
+    # This is not set here due to the 'resources' variable not being accessible in initialize
+    @data = {}
+    @authentication = {}
+    @patch_tags = {}
   end
 
   def self.instances
     @client = OneviewSDK::Client.new(login)
     matches = OneviewSDK::Enclosure.get_all(@client)
     matches.collect do |line|
-      # Puppet.notice("Enclosure: #{line['name']}, URI: #{line['uri']}")
       name = line['name']
       data = line.inspect
-      new(:name   => name,
-          :ensure => :present,
-          :data   => data
-          )
+      new(name: name,
+          ensure: :present,
+          data: data)
     end
-  end
-
-  def self.prefetch(resources)
-    packages = instances
-    resources.keys.each do |name|
-      if provider = packages.find{ |pkg| pkg.name == name }
-        resources[name].provider = provider
-      end
-    end
-  end
-
-  def pretty(arg)
-    return puts arg if arg.instance_of?(String)
-    puts JSON.pretty_generate(arg)
   end
 
   # Provider methods
-
   def exists?
-    # Gets the desired operation to perform
-    state = resource['ensure'].to_s
-
-    # Verify ensure flag and sets environment flags for operations
-    case state
-      when 'present'
-        # Resource itself sends the name of the running proccess
-        enclosure = get_enclosure(resource['data']['name'])
-        enclosure_exists = false
-        enclosure_exists = true if enclosure.first
-        # Checking for and performing potential updates.
-        if enclosure_exists
-          Puppet.notice("#{resource} '#{resource['data']['name']}' located"+
-          " in Oneview Appliance")
-          enclosure_update(resource['data'], enclosure, resource)
-          return true
-        end
-      when 'absent'
-        # Resource itself sends the name of the running proccess
-        enclosure = get_enclosure(resource['data']['name'])
-        enclosure_exists = false
-        enclosure_exists = true if enclosure.first
-        Puppet.notice("#{resource} '#{resource['data']['name']}' not located"+
-        " in Oneview Appliance") if enclosure_exists == false
-        return enclosure_exists
-      else
-        enclosure = find_enclosures(resource['data'])
-        enclosure_exists = false
-        enclosure_exists = true if enclosure
-        return enclosure_exists
-    end
-    @property_hash[:ensure] == :present
+    @data = data_parse
+    empty_data_check
+    %w(from op path value).each { |key| @patch_tags[key] = @data.delete(key) if @data[key] }
+    %w(hostname username password).each { |key| @authentication[key] = @data.delete(key) if @data[key] }
+    !@resourcetype.find_by(@client, @data).empty?
   end
 
   def create
-    data = data_parse(resource['data'])
-    data.delete('new_name') if data['new_name']
-    enclosure = OneviewSDK::Enclosure.new(@client, data)
-    enclosure.add
+    patch_enclosure unless @patch_tags.empty?
+    return true if resource_update(@data, @resourcetype)
+    @data = @data.merge(@authentication)
+    @resourcetype.new(@client, @data).add
     @property_hash[:ensure] = :present
-    @property_hash[:data] = data
+    @property_hash[:data] = @data
+    true
   end
 
   def destroy
-    enclosure = get_enclosure(resource['data']['name'])
-    enclosure.first.remove
+    enclosure = get_single_resource_instance
+    Puppet.notice "\n\n Removing enclosure named: #{enclosure['name']}, with uri: #{enclosure['uri']}\n"
+    enclosure.remove
     @property_hash.clear
+    true
   end
 
   def found
-    # Searches enclosures with data matching the manifest data
-    data = data_parse(resource['data'])
-    data.delete('connectionTemplateUri') if data['connectionTemplateUri'] == nil
-    matches = OneviewSDK::Enclosure.find_by(@client, data)
-    # If matches are found, iterate through them and notify. Else just notify.
-    unless matches.empty?
-       matches.each do |enclosure|
-         Puppet.notice ( "\n\n Found matching enclosure #{enclosure['name']} "+
-         "(URI: #{enclosure['uri']}) on Oneview Appliance\n" )
-      end
-      true
-    else
-      Puppet.notice("\n\n No enclosures with the specified data were found on "+
-      "the Oneview Appliance\n")
-      false
-    end
+    find_resources
   end
 
-  def configured
-      data = data_parse(resource['data'])
-      enclosure = OneviewSDK::Enclosure.new(@client, data)
-      enclosure_exists = enclosure.retrieve! ? true : false
-      enclosure.configuration if enclosure.retrieve!
-      puts "\n\nEnclosure #{enclosure['name']} Updated\n\n" if enclosure_exists
-      puts "\n\nEnclosure #{enclosure['name']} does not exist\n\n" unless enclosure_exists
+  def set_configuration
+    enclosure = get_single_resource_instance
+    enclosure.configuration
+    true
   end
 
-  def retrieved_environmental_configuration
-    data = data_parse(resource['data'])
-    enclosure = OneviewSDK::Enclosure.new(@client, data)
-    # enclosure_exists = enclosure.retrieve! ? true : false
-    if enclosure.retrieve!
-      configuration = enclosure.environmental_configuration
-      puts "\nEnclosure #{enclosure['name']} environmental configuration:\n"
-      pretty configuration
-    end
-    puts "\nEnclosure #{enclosure['name']} does not exist\n" unless enclosure.retrieve!
+  def get_environmental_configuration
+    enclosure = get_single_resource_instance
+    pretty enclosure.environmental_configuration
+    true
   end
 
   def set_environmental_configuration
-      #TODO This endpoint is not yet implemented on the ruby sdk, this should be revisited if/once it is
-      Puppet.notice("\n\n Set environmental configuration is not currently supported via Puppet \n")
+    raise "\n\n Set environmental configuration is not currently supported via the Puppet/Ruby SDK \n"
   end
 
   def set_refresh_state
-      data = data_parse(resource['data'])
-      # Get the refreshState if it exists
-      refresh_state = data['refreshState'] if data['refreshState']
-      data.delete('refreshState') if data['refreshState']
-      Puppet.error("\nThe 'refreshState' must be specified for this operation.\n") unless refresh_state
-      # Get the refreshForceOptions if it exists
-      refresh_force_options = false
-      refresh_force_options = data['refreshForceOptions'] if data['refreshForceOptions']
-      data.delete('refreshForceOptions') if data['refreshForceOptions']
-      # Verify that the enclosure exists
-      enclosure = OneviewSDK::Enclosure.new(@client, data)
-      enclosure_exists = enclosure.retrieve! ? true : false
-      puts "\nEnclosure #{enclosure['name']} does not exist\n" unless enclosure_exists
-      # Sets the refresh state for the enclosure
-      puts "\nSetting refresh state for Enclosure #{enclosure['name']}\n" if enclosure_exists && refresh_state
-      return enclosure.set_refresh_state(refresh_state) if
-      enclosure.retrieve! && refresh_state && refresh_force_options == false
-      return enclosure.set_refresh_state(refresh_state, refresh_force_options) if
-      enclosure.retrieve! && refresh_state && refresh_force_options
-      # return true
+    raise 'A "refreshState" tag is required within data for the current operation.' unless @data['refreshState']
+    refresh_state = @data.delete('refreshState')
+    refresh_force_options = @data.delete('refreshForceOptions') || {}
+    enclosure = get_single_resource_instance
+    enclosure.set_refresh_state(refresh_state, refresh_force_options)
+    true
   end
 
-  def script_retrieved
-      data = data_parse(resource['data'])
-      enclosure = OneviewSDK::Enclosure.new(@client, data)
-      enclosure_exists = enclosure.retrieve! ? true : false
-      puts "\nRetrieving script from enclosure #{enclosure['name']}... \n" if enclosure_exists
-      puts "\nEnclosure #{enclosure['name']} does not exist.\n" unless enclosure_exists
-      enclosure.script if enclosure.retrieve!
+  def get_script
+    enclosure = get_single_resource_instance
+    enclosure.script
+    true
   end
 
-  def retrieved_single_sign_on
-    #TODO This endpoint is not yet implemented on the ruby sdk, this should be revisited if/once it is
-    Puppet.notice("\n\n Single Sign-On is not currently supported via Puppet \n")
+  def get_single_sign_on
+    raise "\n\n Single Sign-On is not currently supported via Puppet \n"
   end
 
-  def retrieved_utilization
-    data = data_parse(resource['data'])
-    # Get the utilization_parameters if it exists
-    utilization_parameters = data['utilization_parameters'] if data['utilization_parameters']
-    data.delete('utilization_parameters') if data['utilization_parameters']
-    Puppet.error("\nThe 'utilization_parameters' must be specified for this operation.\n") unless utilization_parameters
-    # Verify that the enclosure exists
-    enclosure = OneviewSDK::Enclosure.new(@client, data)
-    enclosure_exists = enclosure.retrieve! ? true : false
-    puts "\nEnclosure #{enclosure['name']} does not exist\n" unless enclosure_exists
-    # Retrieve utilization data
-    puts "\nRetrieving utilization data for enclosure '#{enclosure['name']}'\n" if enclosure_exists && utilization_parameters
-    pretty enclosure.utilization(utilization_parameters) if enclosure_exists && utilization_parameters
-    return true if enclosure_exists && utilization_parameters
+  def get_utilization
+    utilization_parameters = @data.delete('utilization_parameters') || {}
+    enclosure = get_single_resource_instance
+    pretty enclosure.utilization(utilization_parameters)
+    true
   end
 
+  def patch_enclosure
+    raise 'The "from" tag is not supported by the current version of the ruby sdk' if @patch_tags['from']
+    raise 'The "op", "path" and "value" tags are required together when used for this operation.' unless
+      @patch_tags['op'] && @patch_tags['path'] && @patch_tags['value']
+    enclosure = get_single_resource_instance
+    enclosure.patch(@patch_tags['op'], @patch_tags['path'], @patch_tags['value'])
+  end
 end
--- lib/puppet/type/common.rb
+++ lib/puppet/type/common.rb
@@ -17,39 +17,65 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../provider', 'login'))
 
 def uri_validation(data)
-  client = OneviewSDK::Client.new(login)
-  uri_recursive_hash(client, data)
+  @client = OneviewSDK::Client.new(login)
+  uri_recursive_hash(data)
   data
 end
 
-def uri_recursive_hash(client, data)
+def uri_recursive_hash(data)
   data.each do |key, value|
+    @value = value
     # in case the key is either an array or hash
-    hash_array_check(client, data[key])
+    hash_array_check(data[key])
     # next if -the uri is already declared- or -the parameter name does not require a uri-
     next if value.to_s[0..6].include?('/rest/') || !(key.to_s.include? 'Uri')
-    # looks for the resource
-    resource = get_class(key).find_by(client, name: value)
-    # fails if resource returns an empty hash (no results)
-    raise 'One or more resources have not been found in the Appliance.' if resource.empty?
-    # replaces the parameter name by its uri
-    data[key] = resource.first.data['uri']
+    data[key] = get_uri(key)
   end
 end
 
 # Broken-down blocks
 
+# Gets the Uri for the resource
+def get_uri(key)
+  parsed_key = special_resources_check(key)
+  # looks for the resource
+  resource = get_class(parsed_key).find_by(@client, name: @value)
+  # fails if resource returns an empty hash (no results)
+  raise "'#{@value}' has not been found in the Appliance." if resource.empty?
+  # replaces the parameter name by its uri
+  resource.first.data['uri']
+end
+
+# Check for special/exceptions to the uri default search
+def special_resources_check(key)
+  return key unless %w(firmwareBaselineUri resourceUri).include?(key)
+  # Assigns the correct key to be used with find_by, and adds 'Uri' to the end of the key
+  # to make it compatible with the get_class method which will be called after this
+  new_key = case key
+            when 'firmwareBaselineUri' then 'FirmwareDriver'
+            when 'resourceUri' then generic_resource_fixer
+            end
+  new_key + 'Uri'
+end
+
+# This is used on generic resources calls like 'resourceUri', where the type is required with the name
+def generic_resource_fixer
+  value_array = @value.strip.split(',').map(&:strip)
+  @value = value_array[0]
+  value_array[1]
+end
+
 # keeps uri_recursive_hash going in case of String, sends to another method
 # in case of arrays or hashes
-def hash_array_check(client, data)
-  uri_recursive_hash(client, data) if data.is_a?(Hash)
-  uri_recursive_array(client, data) if data.is_a?(Array)
+def hash_array_check(data)
+  uri_recursive_hash(data) if data.is_a?(Hash)
+  uri_recursive_array(data) if data.is_a?(Array)
 end
 
 # turns the array into a hash
-def uri_recursive_array(client, data)
+def uri_recursive_array(data)
   data.each do |item|
-    uri_recursive_hash(client, item) if item.is_a?(Hash)
+    uri_recursive_hash(item) if item.is_a?(Hash)
   end
 end
 
