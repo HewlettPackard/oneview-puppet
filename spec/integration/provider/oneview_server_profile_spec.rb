882167d1014263a04f9c5fbe85905249d821e3aa
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-08-18 13:38:34 -0400

    added tests

---
+++ spec/integration/provider/oneview_server_profile_spec.rb
@@ -0,0 +1,67 @@
+################################################################################
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+require 'spec_helper'
+
+provider_class = Puppet::Type.type(:oneview_server_profile).provider(:oneview_server_profile)
+
+describe provider_class do
+  let(:resource) do
+    Puppet::Type.type(:oneview_server_profile).new(
+      name: 'Server Profile',
+      ensure: 'present',
+      data:
+          {
+            'name'              => 'Test Server Profile',
+            'type'              => 'ServerProfileV5',
+            'serverHardwareUri' => '172.18.6.15'
+          }
+    )
+  end
+
+  let(:provider) { resource.provider }
+
+  let(:instance) { provider.class.instances.first }
+
+  before(:each) do
+    provider.exists?
+  end
+
+  context 'given the minimum parameters' do
+    it 'should be an instance of the provider Ruby' do
+      expect(provider).to be_an_instance_of Puppet::Type.type(:oneview_server_profile).provider(:oneview_server_profile)
+    end
+
+    it 'should be able to create the server profile' do
+      expect(provider.create).to be
+    end
+
+    it 'should be able to find the server profile with no filters' do
+      resource['data'] = {}
+      expect(provider.found).to be
+    end
+
+    it 'should be able to update the server profile' do
+      resource['data'] = { 'name' => 'Test Server Profile', 'new_name' => 'Edited SP' }
+      expect(provider.create).to be
+    end
+
+    it 'should be able to delete the server profile' do
+      resource['data'] = { 'name' => 'Edited SP' }
+      expect(provider.destroy).to be
+    end
+  end
+end
---
+++ spec/integration/types/oneview_server_profile_spec.rb
@@ -0,0 +1,85 @@
+################################################################################
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+require 'spec_helper'
+
+type_class = Puppet::Type.type(:oneview_server_profile)
+
+def config
+  {
+    name: 'Server Profile',
+    ensure: 'present',
+    data:
+    {
+      'name' => 'Server Profile'
+    }
+  }
+end
+
+describe type_class do
+  let :params do
+    [
+      :name,
+      :data,
+      :provider
+    ]
+  end
+
+  let :special_ensurables do
+    [
+      :found,
+      :get_available_servers,
+      :get_available_storage_systems,
+      :get_available_storage_system,
+      :get_profile_ports,
+      :get_messages,
+      :get_available_networks,
+      :get_compliance_preview,
+      :get_available_targets
+    ]
+  end
+
+  it 'should have expected parameters' do
+    params.each do |param|
+      expect(type_class.parameters).to include(param)
+    end
+  end
+
+  it 'should accept special ensurables' do
+    special_ensurables.each do |value|
+      expect do
+        described_class.new(name: 'Test',
+                            ensure: value,
+                            data: {})
+      end.to_not raise_error
+    end
+  end
+
+  it 'should require a name' do
+    expect do
+      type_class.new({})
+    end.to raise_error('Title or name must be provided')
+  end
+
+  it 'should require a data hash' do
+    modified_config = config
+    modified_config[:data] = ''
+    expect do
+      type_class.new(modified_config)
+    end.to raise_error(Puppet::ResourceError, 'Parameter data failed on Oneview_server_profile[Server Profile]: '\
+                                               'Validate method failed for class data: Inserted value for data is not valid')
+  end
+end
