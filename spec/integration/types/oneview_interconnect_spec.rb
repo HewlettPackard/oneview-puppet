a6dfcffdc26c251b32e4224673ee4db942158fa5
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-08-19 15:17:57 -0400

    fixed interconnect

--- spec/integration/types/oneview_interconnect_spec.rb
+++ spec/integration/types/oneview_interconnect_spec.rb
@@ -38,16 +38,36 @@ describe type_class do
     ]
   end
 
+  let :special_ensurables do
+    [
+      :found,
+      :get_statistics,
+      :get_name_servers,
+      :reset_port_protection,
+      :update_ports
+    ]
+  end
+
+  it 'should require a name' do
+    expect do
+      type_class.new({})
+    end.to raise_error(Puppet::Error, 'Title or name must be provided')
+  end
+
   it 'should have expected parameters' do
     params.each do |param|
       expect(type_class.parameters).to be_include(param)
     end
   end
 
-  it 'should require a name' do
-    expect do
-      type_class.new({})
-    end.to raise_error('Title or name must be provided')
+  it 'should accept special ensurables' do
+    special_ensurables.each do |value|
+      expect do
+        described_class.new(name: 'Test',
+                            ensure: value,
+                            data: {})
+      end.to_not raise_error
+    end
   end
 
   it 'should require a data hash' do
