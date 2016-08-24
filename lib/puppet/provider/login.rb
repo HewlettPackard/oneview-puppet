9799da0acd31597f183aefb20b7efe673112d0ca
Ana Raquel Campesan <ana.campesan@hpe.com>
2016-08-24 08:39:59 -0400

    fixed complexity

--- lib/puppet/provider/login.rb
+++ lib/puppet/provider/login.rb
@@ -20,15 +20,15 @@ require 'json'
 # The three possible ways of declaring the variables are, respectively:
 def login
   begin
-                  # - Creating a JSON file with the proper fields and setting the environment variable ONEVIEW_AUTH_FILE to its path
+    # - Creating a JSON file with the proper fields and setting the environment variable ONEVIEW_AUTH_FILE to its path
     credentials = if ENV['ONEVIEW_AUTH_FILE']
                     JSON.parse(File.read(File.absolute_path(ENV['ONEVIEW_AUTH_FILE'])), symbolize_names: true)
                   # - Declaring each field as an environment variable
                   elsif ENV['ONEVIEW_URL']
-                    credentials_assignment_env_var(credentials)
+                    environment_credentials
                   # - Placing a JSON file in the manifests directory
                   else
-                    JSON.parse(File.read((File.expand_path('../../../../examples/credentials.json', __FILE__))), symbolize_names: true)
+                    JSON.parse(File.read(File.expand_path('../../../../examples/credentials.json', __FILE__)), symbolize_names: true)
                   end
   rescue
     raise('The Oneview credentials could not be set. Please check the documentation for more information.')
@@ -36,22 +36,20 @@ def login
   credentials_parse(credentials)
 end
 
-# Sets the values according to environment variables
-def credentials_assignment_env_var(credentials)
-  credentials[:url]         = ENV['ONEVIEW_URL']
-  credentials[:ssl_enabled] = ENV['ONEVIEW_SSL_ENABLED']
-  credentials[:log_level]   = ENV['ONEVIEW_LOG_LEVEL'] || 'info'
-  credentials[:api_version] = ENV['ONEVIEW_API_VERSION'] || 200
-  if ENV['ONEVIEW_TOKEN']
-    credentials[:token] = ENV['ONEVIEW_TOKEN']
-  else
-    credentials[:user]     = ENV['ONEVIEW_USER']
-    credentials[:password] = ENV['ONEVIEW_PASSWORD']
-  end
-  credentials
+# Returns the credentials set by environment variables
+def environment_credentials
+  {
+    url:         ENV['ONEVIEW_URL'],
+    ssl_enabled: ENV['ONEVIEW_SSL_ENABLED'],
+    log_level:   ENV['ONEVIEW_LOG_LEVEL'] || 'info',
+    api_version: ENV['ONEVIEW_API_VERSION'] || 200,
+    token:       ENV['ONEVIEW_TOKEN'] || nil,
+    user:        ENV['ONEVIEW_USER'] || nil,
+    password:    ENV['ONEVIEW_PASSWORD'] || nil
+  }
 end
 
-# Converts strings into booleans
+# Converts strings into booleans and returns the credentials ready to go
 def credentials_parse(credentials)
   credentials.each do |key, value|
     credentials[key] = false if value == 'false'
