52ed04fd7cef0d59d398dd4a96b1f2da2728ce4a
Felipe Bulsoni <fgbulsoni@hpe.com>
2017-01-20 15:29:02 -0200

    Extending support for API300 on Synergy and C7000 for the Oneview_logical_interconnect resource

--- lib/puppet/provider/oneview_logical_interconnect/c7000.rb
+++ lib/puppet/provider/oneview_logical_interconnect/c7000.rb
@@ -14,19 +14,14 @@
 # limitations under the License.
 ################################################################################
 
-require_relative '../login'
-require_relative '../common'
-require 'oneview-sdk'
+require_relative '../oneview_resource'
 
-Puppet::Type.type(:oneview_logical_interconnect).provide(:oneview_logical_interconnect) do
+Puppet::Type::Oneview_logical_interconnect.provide :c7000, parent: Puppet::OneviewResource do
+  desc 'Provider for OneView Logical Interconnects using the C7000 variant of the OneView API'
+
+  confine true: login[:hardware_variant] == 'C7000'
+
   mk_resource_methods
-
-  def initialize(*args)
-    super(*args)
-    @client = OneviewSDK::Client.new(login)
-    @resourcetype = OneviewSDK::LogicalInterconnect
-    @data = {}
-  end
 
   def exists?
     @data = data_parse
