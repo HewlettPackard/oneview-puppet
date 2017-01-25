01571bc50b9f0bc04885a314adce63bbb8aa08cd
Felipe Bulsoni <fgbulsoni@hpe.com>
2017-01-25 18:08:48 -0200

    Removing change to managed san unit tests

--- spec/unit/provider/oneview_managed_san_c7000_spec.rb
+++ spec/unit/provider/oneview_managed_san_c7000_spec.rb
@@ -89,7 +89,6 @@ describe provider_class, unit: true do
     end
 
     it 'should be able to run through self.instances' do
-      allow(resourcetype).to receive(:find_by).and_return([test])
       expect(instance).to be
     end
 
