5856145a28fd1e92f68a23d35607ec427a2f29e0
Mariana Kreisig <mariana.kreisig@hpe.com>
2017-01-26 10:25:06 -0200

    Updates synergy unit test to prevent it from running on api 200

--- spec/unit/provider/oneview_volume_synergy_spec.rb
+++ spec/unit/provider/oneview_volume_synergy_spec.rb
@@ -23,7 +23,7 @@ api_version = login[:api_version] || 200
 resource_name = 'Volume'
 resourcetype = Object.const_get("OneviewSDK::API#{api_version}::Synergy::#{resource_name}") unless api_version < 300
 
-describe provider_class, unit: true do
+describe provider_class, unit: true, if: api_version >= 300 do
   include_context 'shared context'
 
   let(:resource) do
