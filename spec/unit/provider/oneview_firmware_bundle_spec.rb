4b8b1898c8bd9ccb12d1b12e143ec0f02212147b
Abilio Parada <abilio.parada@hpe.com>
2017-02-17 16:51:46 -0200

    Fix test Firmware Bundle

--- spec/unit/provider/oneview_firmware_bundle_spec.rb
+++ spec/unit/provider/oneview_firmware_bundle_spec.rb
@@ -17,7 +17,10 @@
 require 'spec_helper'
 
 provider_class = Puppet::Type.type(:oneview_firmware_bundle).provider(:c7000)
-resourcetype = OneviewSDK::FirmwareBundle
+
+api_version = login[:api_version] || 200
+resource_name = 'FirmwareBundle'
+resourcetype = Object.const_get("OneviewSDK::API#{api_version}::C7000::#{resource_name}") unless api_version < 300
 
 describe provider_class, unit: true do
   include_context 'shared context'
@@ -29,7 +32,8 @@ describe provider_class, unit: true do
       data:
           {
             'firmware_bundle_path' => './spec/support/test_firmware_bundle.spp'
-          }
+          },
+      provider: 'c7000'
     )
   end
 
