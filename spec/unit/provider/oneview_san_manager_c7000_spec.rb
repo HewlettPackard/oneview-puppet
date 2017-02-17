319ea3ccf853e8bbaacff99217e2bbec836974bd
Abilio Parada <abilio.parada@hpe.com>
2017-02-17 16:52:13 -0200

    Fix test SAN Manager

--- spec/unit/provider/oneview_san_manager_c7000_spec.rb
+++ spec/unit/provider/oneview_san_manager_c7000_spec.rb
@@ -22,7 +22,7 @@ api_version = login[:api_version] || 200
 resource_name = 'SANManager'
 resourcetype = Object.const_get("OneviewSDK::API#{api_version}::C7000::#{resource_name}") unless api_version < 300
 
-describe provider_class, unit: true do
+describe provider_class, unit: true, if: login[:api_version] >= 300 do
   include_context 'shared context'
 
   context 'given the min parameters' do
