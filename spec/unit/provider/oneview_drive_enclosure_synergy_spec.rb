74150f0da3fce38f2ff84f1637b667e51ae0c95c
Abilio Parada <abilio.parada@hpe.com>
2017-02-20 12:36:40 -0300

    Fix issues pointed in review Drive Enclosure

--- spec/unit/provider/oneview_drive_enclosure_synergy_spec.rb
+++ spec/unit/provider/oneview_drive_enclosure_synergy_spec.rb
@@ -18,10 +18,11 @@ require 'spec_helper'
 
 provider_class = Puppet::Type.type(:oneview_drive_enclosure).provider(:synergy)
 api_version = login[:api_version] || 200
-resourcetype = OneviewSDK.resource_named(:DriveEnclosure, api_version, 'Synergy')
 
 describe provider_class, unit: true, if: login[:api_version] >= 300 do
   include_context 'shared context'
+
+  resourcetype = OneviewSDK.resource_named(:DriveEnclosure, api_version, 'Synergy')
 
   let(:resource) do
     Puppet::Type.type(:oneview_drive_enclosure).new(
