d47bf7fd45d8d1bd63db25215c79ab7e73805159
Mariana Kreisig <mariana.kreisig@hpe.com>
2017-02-22 10:12:37 -0300

    Removed Golden Image fixture

--- spec/integration/provider/image_streamer_golden_image_spec.rb
+++ spec/integration/provider/image_streamer_golden_image_spec.rb
@@ -17,6 +17,7 @@
 require 'spec_helper'
 
 provider_class = Puppet::Type.type(:image_streamer_golden_image).provider(:image_streamer)
+golden_image_path = login[:golden_image_path] || './spec/support/golden_image.zip'
 
 describe provider_class do
   let(:resource) do
@@ -90,7 +91,7 @@ describe provider_class do
             {
               'name'              => 'Golden_Image_2',
               'description'       => 'Golden image added from the file that is uploaded from a local drive',
-              'golden_image_path' => 'spec/support/fixtures/integration/provider/golden_image.zip'
+              'golden_image_path' => golden_image_path
             }
       )
     end
