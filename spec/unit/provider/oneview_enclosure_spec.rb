5a66463170e0e6e2fe1f11f9a34254f7ed1fe160
Jyothis <jyothis@hpe.com>
2018-05-17 15:28:04 +0530

    Unit tests updated for ruby v2.4 and above.

--- spec/unit/provider/oneview_enclosure_spec.rb
+++ spec/unit/provider/oneview_enclosure_spec.rb
@@ -53,6 +53,7 @@ describe provider_class, unit: true do
 
   context 'given the min parameters' do
     before(:each) do
+      allow_any_instance_of(resource_type).to receive(:retrieve!).and_return(true)
       allow(resource_type).to receive(:find_by).with(anything, resource['data']).and_return([test])
       provider.exists?
     end
