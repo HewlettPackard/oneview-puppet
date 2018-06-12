1200686524cebe2c0b71780a452c9be3e5a765d4
Madhav <madhavb@hpe.com>
2018-06-12 02:26:53 -0400

    New resource -os deployment plans

---
+++ lib/puppet/type/oneview_os_deployment_plan.rb
@@ -0,0 +1,41 @@
+################################################################################
+# (C) Copyright 2016-2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+Puppet::Type.newtype(:oneview_os_deployment_plan) do
+  desc "Oneview's OS Deployment plan"
+
+  ensurable do
+    defaultvalues
+
+    # :nocov:
+    # Get methods
+    newvalue(:found) do
+      provider.found
+    end
+    # :nocov:
+  end
+
+  newparam(:name, namevar: true) do
+    desc 'OS Deployment Plan name'
+  end
+
+  newproperty(:data) do
+    desc 'Volume data hash containing all specifications for the system'
+    validate do |value|
+      raise 'Inserted value for data is not valid' unless value.class == Hash
+    end
+  end
+end
