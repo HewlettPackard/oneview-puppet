f928a29aa508820a733d2c7b3bdffd2f2eda4bb4
Chebrolu <bala-sai-harika.chebrolu@hpe.com>
2020-04-17 17:01:55 +0530

    fix whitespace issue

--- spec/integration/type/oneview_hypervisor_manager_spec.rb
+++ spec/integration/type/oneview_hypervisor_manager_spec.rb
@@ -5,13 +5,13 @@ type_class = Puppet::Type.type(:oneview_hypervisor_manager)
 def hm_config
   {
     name: 'test_hm',
-    ensure: 'present'
+    ensure: 'present',
     data:
-      {
+        {
         name     => '172.18.13.11',
         username => 'dcs',
         password => 'dcs'
-      }
+        }
   }
 end
 
