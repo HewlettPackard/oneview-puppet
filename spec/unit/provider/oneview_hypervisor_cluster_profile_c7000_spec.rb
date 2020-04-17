d0e078f1a0600f418a59118018df073f597ddab9
Chebrolu <bala-sai-harika.chebrolu@hpe.com>
2020-04-17 16:14:14 +0530

    fix in UTs

--- spec/unit/provider/oneview_hypervisor_cluster_profile_c7000_spec.rb
+++ spec/unit/provider/oneview_hypervisor_cluster_profile_c7000_spec.rb
@@ -17,6 +17,8 @@
 require 'spec_helper'
 
 provider_class = Puppet::Type.type(:oneview_hypervisor_cluster_profile).provider(:c7000)
+api_version = 800
+resource_type = OneviewSDK.resource_named(:HypervisorClusterProfile, api_version, :C7000)
 
 describe provider_class, unit: true do
   include_context 'shared context Hypervisor Cluster Profile'
