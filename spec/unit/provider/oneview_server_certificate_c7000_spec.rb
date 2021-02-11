690f27c6325869cfa787c36b13b7d1aaa57cc715
VenkateshRavula <venkatesh.ravula@hpe.com>
2021-02-11 15:06:57 +0530

    change UT

--- spec/unit/provider/oneview_server_certificate_c7000_spec.rb
+++ spec/unit/provider/oneview_server_certificate_c7000_spec.rb
@@ -98,7 +98,7 @@ describe provider_class, unit: true do
            'base64Data' => 'some certificate data'
          }]
       }
-      allow_any_instance_of(resource_type).to receive(:get_certificate).and_return(expected_output)
+      allow_any_instance_of(resource_type).to receive(:get_certificate).and_return()
       allow_any_instance_of(resource_type).to receive(:update).and_return(test)
       expect(provider.create_or_update).to be
     end
